07ffb0bd3720a621c159c45e518dcde02086eee4
AbdulrahmanShaaban <aboodxs3a@gmail.com>
2026-05-26 05:59:34 +0300

    fix: update api base url

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://auth-system-sage.vercel.app/api',
+  baseURL: 'https://auth-system-backend-blond.vercel.app/api',
   withCredentials: true,
 });
